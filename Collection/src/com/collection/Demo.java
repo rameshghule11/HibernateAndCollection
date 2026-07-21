dc5f3a4d368964bdc91dfef887d53b8c5d629625
rameshghule11 <rameshghule112002@gmail.com>
2026-07-21 16:46:19 +0530

    Collection add

---
+++ Collection/src/com/collection/Demo.java
@@ -0,0 +1,28 @@
+package com.collection;
+
+import java.util.ArrayList;
+
+public class Demo {
+
+	public static void main(String args [] ) {
+		
+		ArrayList<String> al=new ArrayList<String>();
+		
+			al.add("Banti");
+			al.add("Dnyanu");
+			al.add("kalu");
+			al.add("ash");
+			
+			System.out.println("String Values: "+al);
+			
+			ArrayList<Float> fl=new  ArrayList <Float>();
+			
+			fl.add(12.2f);
+			fl.add(13.4f);
+			fl.add(99.0f);
+			fl.add(95.99f);
+			
+			System.out.println("Float Value:" +fl);
+			 
+	}
+}
