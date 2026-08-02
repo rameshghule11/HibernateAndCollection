b139d5325dd597d7157d7f7df7a69bbbdb9d94c4
rameshghule11 <rameshghule112002@gmail.com>
2026-08-02 23:54:57 +0530

    more code added

--- Collection/src/com/collection/LinkedListDemo.java
+++ Collection/src/com/collection/LinkedListDemo.java
@@ -36,5 +36,17 @@ public class LinkedListDemo {
 //			System.out.println("List2: "+itrs.next());
 //		}
 		
+		LinkedList<String>list2=new LinkedList<String>();
+		list2.add("banti");
+		list2.add("Ramesh");
+		list2.add("Ash");
+		list2.add("Doremon");
+		list2.add("Pikachu");
+		
+		Iterator<String>itr2=list2.descendingIterator();
+		while(itr2.hasNext()) {
+			System.out.println(itr2.next());
+		}
+		
 	}
 }
---
+++ Collection/src/com/map/HashMapDemo.java
@@ -0,0 +1,11 @@
+package com.map;
+
+import java.util.HashMap;
+
+public class HashMapDemo {
+
+	public static void main(String[] args) {
+		
+		HashMap<K, V>();
+	}
+}
