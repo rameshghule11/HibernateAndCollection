01a5a528477b527bb3a9c47260cc78326c3fac22
rameshghule11 <rameshghule112002@gmail.com>
2026-08-03 13:10:03 +0530

    HashMap Defination Add

--- Collection/src/com/map/HashMapDemo.java
+++ Collection/src/com/map/HashMapDemo.java
@@ -6,6 +6,29 @@ public class HashMapDemo {
 
 	public static void main(String[] args) {
 		
+		/*
+		Map is an Interface which is prsent in java.util 
+		map is used to store data in key-value pairs
+		map Provides 4 impliments classes 
+		1HashMap
+		2Linkedhashmap
+		3TreeMap
+		4HashTable
+		
+		HashMap is an implimented class of map Interface wich is prsent in java.util.package
+		hashMap Store the data in form key-value pairs 
+		 -- Each key-value pairs is known as an Entery
+		 Syntax:- HasMap<datatype,DataType> map=new HashMap<datatype,datatype>();
+		 Syntax:- HasMap<key,Value> map=new HashMap<Key, Value();
+		HashMap can Contain duplicate values 
+		HashMap can not contain duplicate key values
+		 if we make key duplicate it will remove Existing Records 
+		HashMap can not followed Insertion order
+		HashMap can contain multiple null values 
+		HashMap can contain Single null key only
+		
+		*/
+		
 		HashMap<String, String> map=new HashMap<String,String>();
 		
 		map.put("banti","javaDevloper");
