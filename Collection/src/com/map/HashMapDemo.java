3559b01b4835c105ab69cd8da3b47b20bbac2f5a
rameshghule11 <rameshghule112002@gmail.com>
2026-08-03 12:50:41 +0530

    HashMap Basic code Add

--- Collection/src/com/map/HashMapDemo.java
+++ Collection/src/com/map/HashMapDemo.java
@@ -6,6 +6,14 @@ public class HashMapDemo {
 
 	public static void main(String[] args) {
 		
-		HashMap<K, V>();
+		HashMap<String, String> map=new HashMap<String,String>();
+		
+		map.put("banti","javaDevloper");
+		map.put("kalpesh","Teacher");
+		map.put("Banti","Gamer");
+		map.put("Banti","vloger");
+		
+		System.out.println(map);
+		
 	}
 }
