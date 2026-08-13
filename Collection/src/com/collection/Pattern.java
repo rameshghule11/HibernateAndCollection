4952886917fef1fb32aab4c4a5322672b84e6d87
rameshghule11 <rameshghule112002@gmail.com>
2026-08-13 20:13:12 +0530

    Collection complate

---
+++ Collection/src/com/collection/Pattern.java
@@ -0,0 +1,23 @@
+package com.collection;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		
+		int n=5;
+		
+		for(int i=1;i<=n;i++) {
+			
+			for(int j=1;j<=i;j++) {
+				if(j==1|| j==i||i==5) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			
+			System.out.println();
+		}
+	
+	}
+}
